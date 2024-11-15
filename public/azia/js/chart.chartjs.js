1534c8a6b5c74404dad8ecb632372dd19bfac813
Mawla <agielsingo@gmail.com>
2024-11-15 21:59:54 +0700

    edit superuser

--- public/azia/js/chart.chartjs.js
+++ public/azia/js/chart.chartjs.js
@@ -1,7 +1,7 @@
 $(function(){
   'use strict';
 
-/* BAR CHART PERBANDINGAN */
+/* BAR CHART PERBANDINGANN */
 const chartBarElement = document.getElementById('chartBar5');
 if (chartBarElement) {
     const labels = JSON.parse(chartBarElement.getAttribute('data-labels'));
